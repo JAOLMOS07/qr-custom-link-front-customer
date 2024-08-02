51c784e37aeff090e47f6fb2635a9d86524ffd31
JAOLMOS07 <jhogerolmos10@gmail.com>
2024-08-02 08:39:04 -0500

    cambios generales

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -4,15 +4,15 @@ export const environment = {
   pathConfig: "./assets/config",
   apiSuffix: "/api/v1/",
   companyAppUrl:
-    "https://az-qrcustomlink-back-company-appservice-dev.azurewebsites.net",
+    "https://az-qrcustomlink-back-company-appservice-uat.azurewebsites.net",
   contentAppUrl:
-    "https://az-qrcustomlink-back-content-appservice-dev.azurewebsites.net",
+    "https://az-qrcustomlink-back-content-appservice-uat.azurewebsites.net",
   orderAppUrl:
-    "https://az-qrcustomlink-back-order-appservice-dev.azurewebsites.net",
+    "https://az-qrcustomlink-back-order-appservice-uat.azurewebsites.net",
   linkAppsUrl:
-    "https://az-qrcustomlink-back-link-appservice-dev.azurewebsites.net",
+    "https://az-qrcustomlink-back-link-appservice-uat.azurewebsites.net",
   identityAppUrl:
-    "https://az-qrcustomlink-back-identity-appservice-dev.azurewebsites.net",
+    "https://az-qrcustomlink-back-identity-appservice-uat.azurewebsites.net",
   frontIdentityAppUrl: "https://jolly-water-0eb08d710.4.azurestaticapps.net",
 };
 
